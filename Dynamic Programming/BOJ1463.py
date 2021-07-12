879280952cf07357100cc5b2346d080bc2e620b9
softrain <yurimplus@naver.com>
2021-07-12 16:36:41 +0900

    DP

---
+++ Dynamic Programming/BOJ1463.py
@@ -0,0 +1,13 @@
+N = int(input())
+
+dp = [0] * (N + 1)
+for i in range(1, N + 1):
+    if i == 1:
+        dp[i] = 0
+    else:
+        dp[i] = dp[i - 1] + 1
+        if i % 2 == 0:
+            dp[i] = min(dp[i], dp[i // 2] + 1)
+        if i % 3 == 0:
+            dp[i] = min(dp[i], dp[i // 3] + 1)
+print(dp[N])
