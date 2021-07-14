3eb7930c5c19701c91aa0867fa4e02383835b949
softrain <yurimplus@naver.com>
2021-07-14 14:23:45 +0900

    9095

---
+++ Dynamic Programming/BOJ9095.py
@@ -0,0 +1,9 @@
+T = int(input())
+
+
+for i in range(T):
+    dp = [0, 1, 2, 4]
+    n = int(input())
+    for i in range(4, n + 1):
+        dp.append(dp[i - 1] + dp[i - 2] + dp[i - 3])
+    print(dp[n])
