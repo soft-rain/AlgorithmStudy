0bcc0922410737d29bbcc9aedcc0b4ee9ca9e3e3
softrain <yurimplus@naver.com>
2021-07-23 14:07:00 +0900

    10814 나이순 정렬

---
+++ ETC/BOJ10814.py
@@ -0,0 +1,11 @@
+import sys
+
+N = int(sys.stdin.readline())
+arr = []
+for i in range(N):
+    arr.append(list(sys.stdin.readline().split()))
+arr.sort(key=lambda x: int(x[0]))
+for i in range(N):
+    print(arr[i][0], arr[i][1])
+key = lambda x: x * 5
+print(key(8))
