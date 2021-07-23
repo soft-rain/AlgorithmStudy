d625a2c4b94e45c5c4c90171be70b63a82a8bc74
softrain <yurimplus@naver.com>
2021-07-23 14:27:21 +0900

    .

--- ETC/BOJ10814.py
+++
@@ -1,9 +0,0 @@
-import sys
-
-N = int(sys.stdin.readline())
-arr = []
-for i in range(N):
-    arr.append(list(sys.stdin.readline().split()))
-arr.sort(key=lambda x: int(x[0]))
-for i in range(N):
-    print(arr[i][0], arr[i][1])
--- ETC/BOJ11650.py
+++
@@ -1,9 +0,0 @@
-import sys
-
-N = int(sys.stdin.readline())
-arr = []
-for i in range(N):
-    arr.append(list(map(int, sys.stdin.readline().split())))
-arr.sort()
-for i in range(N):
-    print(arr[i][0], arr[i][1])
