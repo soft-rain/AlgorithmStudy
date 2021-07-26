3ea218cc06103f233b2d1e60228c740f1cb42c2c
softrain <yurimplus@naver.com>
2021-07-27 03:48:36 +0900

    10828 스택

---
+++ Stack/BOJ10828.py
@@ -0,0 +1,26 @@
+import sys
+
+N = int(sys.stdin.readline())
+
+stack = []
+for i in range(N):
+    a = list(sys.stdin.readline().split())
+    if a[0] == "push":
+        stack.append(a[1])
+    elif a[0] == "pop":
+        if len(stack) != 0:
+            print(stack.pop())
+        else:
+            print(-1)
+    elif a[0] == "size":
+        print(len(stack))
+    elif a[0] == "empty":
+        if len(stack) != 0:
+            print(0)
+        else:
+            print(1)
+    elif a[0] == "top":
+        if len(stack) != 0:
+            print(stack[len(stack) - 1])
+        else:
+            print(-1)
