5e659a392cfc41ef5ef3f69b3be4e2c06915b4f2
softrain <yurimplus@naver.com>
2021-07-30 20:19:19 +0900

    10808 알파벳 개수

---
+++ BOJ10808.py
@@ -0,0 +1,35 @@
+alphabet = [
+    "a",
+    "b",
+    "c",
+    "d",
+    "e",
+    "f",
+    "g",
+    "h",
+    "i",
+    "j",
+    "k",
+    "l",
+    "m",
+    "n",
+    "o",
+    "p",
+    "q",
+    "r",
+    "s",
+    "t",
+    "u",
+    "v",
+    "w",
+    "x",
+    "y",
+    "z",
+]
+
+st = list(input())
+print(st)
+
+for i in range(26):
+    ans = st.count(alphabet[i])
+    print(ans, end=" ")
