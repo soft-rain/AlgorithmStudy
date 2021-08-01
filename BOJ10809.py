e231ab6d3a75366e48c45f488da5d7679d55dbb8
softrain <yurimplus@naver.com>
2021-08-02 02:11:58 +0900

    10809 알파벳 찾기

---
+++ BOJ10809.py
@@ -0,0 +1,37 @@
+import sys
+
+alphabet = [
+    "a",
+    "b",
+    "c",
+    "d",
+    "e",
+    "f",
+    "g",
+    "h",
+    "i",
+    "j",
+    "k",
+    "l",
+    "m",
+    "n",
+    "o",
+    "p",
+    "q",
+    "r",
+    "s",
+    "t",
+    "u",
+    "v",
+    "w",
+    "x",
+    "y",
+    "z",
+]
+st = list(sys.stdin.readline())
+
+for i in alphabet:
+    if i in st:
+        print(st.index(i), end=" ")
+    else:
+        print(-1, end=" ")
