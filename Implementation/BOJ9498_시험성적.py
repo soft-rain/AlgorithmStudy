a021a8a3e34976b86044ae04004833ea0f6509f0
softrain <yurimplus@naver.com>
2022-01-05 23:12:21 +0900

    [BOJ] 9498 시험 성적

---
+++ Implementation/BOJ9498_시험성적.py
@@ -0,0 +1,11 @@
+n = int(input())
+if n <= 100 and n >= 90:
+    print("A")
+elif n < 90 and n >= 80:
+    print("B")
+elif n < 80 and n >= 70:
+    print("C")
+elif n < 70 and n >= 60:
+    print("D")
+else:
+    print("F")
